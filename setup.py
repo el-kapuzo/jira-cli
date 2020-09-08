5017432acd93f18ebdb15c915ce970b236e2f47a
Daniel Pohl <daniel.pohl@up2parts.com>
2020-09-08 12:10:52 +0200

    INIT: project structure and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt"), "r"
+) as requirementsFile:
+    requirements = [line for line in requirementsFile]
+
+setuptools.setup(
+    name="jira-cli", # TODO find good name
+    version="0.0.1a",
+    author="el-kap",
+    author_email="cherrybomb@up2parts.com",
+    python_requires=">=3.6,<3.7",
+    description="CL-Commands to interact with jira",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages("src")
+)
