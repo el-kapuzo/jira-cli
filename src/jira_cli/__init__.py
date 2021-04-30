49946cffe16d9f967f7bd53e540d8b19e3af9075
Daniel Pohl <daniel.pohl@up2parts.com>
2021-04-30 10:34:31 +0200

    [ADD] debug statements

--- src/jira_cli/__init__.py
+++ src/jira_cli/__init__.py
@@ -4,12 +4,16 @@ import pkgutil
 
 
 def _importModules(*path):
+    print(f"Try to import modules from {path}")
     for module in pkgutil.iter_modules(
         [os.path.join(os.path.abspath(os.path.dirname(__file__)), *path)]
     ):
         if not (module.ispkg):
+            pathWithDots = ".".join(path)
+            importPath = f"jira_cli.{pathWithDots}.{module.name}"
+            print(f"IMPORTING: {importPath}")
             importlib.import_module(
-                f".{'.'.join(*path)}.{module.name}", package=__name__
+                f".{'.'.join(path)}.{module.name}", package=__name__
             )
 
 
@@ -17,5 +21,5 @@ def _importModules(*path):
 # is executed for each policy
 # This way, you can just add or delete policies
 # this snipped will manage this automatically for you
-for path in (("commands"), ("commands", "taks"), ("commands", "comments")):
+for path in (("commands",),): #, ("commands", "taks"), ("commands", "comments")):
     _importModules(*path)
