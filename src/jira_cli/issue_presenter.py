933acbe3abf9910f466c1f4f6b5d7747d628703f
Daniel Pohl <daniel.pohl@up2parts.com>
2021-05-27 16:03:33 +0200

    [UPDATE] print issues with rich

--- src/jira_cli/issue_presenter.py
+++ src/jira_cli/issue_presenter.py
@@ -6,13 +6,13 @@ class IssuePresenter:
         self.color_map = {"Bug": "red", "In Progress": "blue", "Done": "green"}
 
     def print_issue(self, issue):
-        color = None
+        color = "white"
         issue_status = issue.fields.status.name
         issue_type = str(issue.fields.issuetype)
         if issue_status == "Done":
-            color = self.color_map.get("Done")
+            color = self.color_map.get("Done", "white")
         else:
-            color = self.color_map.get(issue_status)
+            color = self.color_map.get(issue_status, "white")
             if issue_type == "Bug":
-                color = self.color_map.get(issue_type)
+                color = self.color_map.get(issue_type, "white")
         rich.print(f"    [{color}][bold]{issue.key}[/bold]: {issue.fields.summary}")
