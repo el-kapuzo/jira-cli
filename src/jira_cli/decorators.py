ed108115dd839f24955addc0c18e8eda1f2d5d6c
Daniel Pohl <daniel.pohl@up2parts.com>
2021-06-07 11:14:24 +0200

    [REMOVE] unneeded decorators

--- src/jira_cli/decorators.py
+++ src/jira_cli/decorators.py
@@ -1,23 +1,6 @@
 from .application import Application
-from .completion import JiraCompleter
 
 
 def resource(cls):
     Application.resources[cls.__name__.lower()] = cls
     return cls
-
-
-def command(name):
-    def decorator(fun):
-        Application.commands[name] = fun
-        return fun
-
-    return decorator
-
-
-def completion_provider(name):
-    def decorator(fun):
-        JiraCompleter.completion_factories[name] = fun
-        return fun
-
-    return decorator
