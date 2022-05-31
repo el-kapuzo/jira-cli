a12a35c33b1b81275edf10c6f043265ffc3553b0
El Kapuzo <el.kapuzo@mailbox.org>
2022-05-31 11:09:25 +0200

    refactor(commands): add presenter decorator to resource-mixin

--- src/jira_cli/commands/resource.py
+++ src/jira_cli/commands/resource.py
@@ -3,6 +3,14 @@ from jira_cli.completion import FuzzyNestedCompleter
 
 
 class ResourceMixin:
+    @property
+    def completion_providers(self):
+        return getattr(self.__class__, "_completion_providers", dict())
+
+    @property
+    def command_handlers(self):
+        return getattr(self.__class__, "_command_handlers", dict())
+
     def get_completer(self, application):
         completer_map = {
             name: completion_provider(application)
@@ -19,6 +27,8 @@ class ResourceMixin:
     @classmethod
     def command(cls, name):
         def decorator(fun):
+            if not hasattr(cls, "command_handlers"):
+                cls.command_handlers = {}
             cls.command_handlers[name] = fun
             return fun
 
@@ -27,7 +37,17 @@ class ResourceMixin:
     @classmethod
     def completion_provider(cls, name):
         def decorator(fun):
+            if not hasattr(cls, "completion_providers"):
+                cls.completion_providers = {}
             cls.completion_providers[name] = fun
             return fun
 
         return decorator
+
+    @classmethod
+    def presenter(cls, name):
+        def decorator(fun):
+            if not hasattr(cls, "presenters"):
+                cls.presenters = {}
+            cls.presenter[name] = fun
+            return fun
